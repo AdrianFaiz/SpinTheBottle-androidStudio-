56e7e6ecbad932c3d0cd347b7d4fe7e0461f73b6
adrian faiz villena <adrianfaizvillena929@gmail.com>
2024-03-28 23:39:01 +0800

    Initial commit

---
+++ app/src/main/java/com/codinginflow/spinthebottle/home.java
@@ -0,0 +1,19 @@
+package com.codinginflow.spinthebottle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+    private void hide() {
+    }
+}
